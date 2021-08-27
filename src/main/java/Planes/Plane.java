d9476b12d2b2488b430f6d5d1f92d06e1b8cf065
Stephen <stephendcorcoran@gmail.com>
2021-08-27 18:33:08 +0100

    added capacity to plane

--- src/main/java/Planes/Plane.java
+++ src/main/java/Planes/Plane.java
@@ -2,8 +2,16 @@ package Planes;
 
 public enum Plane {
 
-    BOEING747;
-    
+    BOEING747(747);
 
+    private int capacity;
+
+    Plane(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
 
