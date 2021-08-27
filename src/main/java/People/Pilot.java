e8a63c8e8690c24d1464d0ffa133039c3a0634e2
Stephen <stephendcorcoran@gmail.com>
2021-08-27 17:25:14 +0100

    Pilot

--- src/main/java/People/Pilot.java
+++ src/main/java/People/Pilot.java
@@ -1,4 +1,15 @@
 package People;
 
-public class Pilot {
+public class Pilot extends CabinCrewMember {
+
+    private String pilotLicenceNumber;
+
+    public Pilot (String name, int rank, String pilotLicenceNumber ) {
+        super(name, rank);
+        this.pilotLicenceNumber = pilotLicenceNumber;
+    }
+
+    public String getPilotLicenceNumber() {
+        return pilotLicenceNumber;
+    }
 }
